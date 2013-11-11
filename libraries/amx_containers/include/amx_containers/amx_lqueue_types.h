9f25e8da29167afe390d21202ec816bee873b037
Obelix1 <peter.deherdt@telenet.be>
2013-11-11 21:55:19 +0100

    Clean-up Documentation Removed lcov generated file

--- libraries/amx_containers/include/amx_containers/amx_lqueue_types.h
+++ libraries/amx_containers/include/amx_containers/amx_lqueue_types.h
@@ -8,8 +8,33 @@ extern "C"
 
 #include <amx_containers/amx_llist.h>
 
-typedef amx_llist_t amx_lqueue_t ;
+/**
+ @file
+ @brief
+ Ambiorix linked list types header file
+*/
+
+/**
+ @ingroup amx_lqueue
+ @brief
+ The linked queue structure.
+ */
+typedef amx_llist_t amx_lqueue_t;
+
+/**
+ @ingroup amx_lqueue
+ @brief
+ The linked queue iterator structure.
+ */
 typedef amx_llist_it_t amx_lqueue_it_t;
+
+/**
+ @ingroup amx_lqueue
+ @brief
+ Definition of the item delete function.
+
+ A pointer to a delete function is used in the following functions @ref amx_delete_lqueue, @ref amx_clean_lqueue
+ */
 typedef amx_llist_it_delete_t amx_lqueue_it_delete_t;
 
 #ifdef __cplusplus
